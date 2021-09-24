b6befc6a2da3731dfadfba38b9d98a4a32f22601
sirsnoop456 <agilsaputra2601@gmail.com>
2021-09-24 23:10:01 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import {Hasil, ListCategories,NavbarComponent} from "./component";
+import { Hasil, ListCategories, Menus, NavbarComponent } from "./component";
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className = "mt-2">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+      categoriYangDipilih: "Makanan",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products?category.nama=" + this.state.categoriYangDipilih)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  changecategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: [],
+    });
+    axios
+      .get(API_URL + "products?category.nama=" + value)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { menus, categoriYangDipilih } = this.state;
+    console.log(this.state.menus);
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-2">
+          <Container fluid>
+            <Row>
+              <ListCategories
+                changecategory={this.changecategory}
+                categoriYangDipilih={categoriYangDipilih}/>
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus &&
+                    menus.map((menu) => <Menus key={menu.id} menu={menu} />)}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
-
